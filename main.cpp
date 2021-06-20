4299ed15fcf0a18e834016d07cbd0162088145d8
Dmytro Malovanyy <dmitrymalovanyy@gmail.com>
2021-06-20 19:32:43 +0300

    toml

--- main.cpp
+++ main.cpp
@@ -1,4 +1,30 @@
+
+#include <iostream>
+
+//C libs
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <toml++/toml.h>
+
+
+
+struct APIParams {
+    const char* host;
+    const char* port;
+};
+
+void parseTomlConfig( const char* confiPath ) {
+    toml::table table;
+    try {
+        table = toml::parse_file( confiPath );
+    } catch (const toml::parse_error& err) {
+        std::cerr << "Some errors while parsing toml occured" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+
 
 int main(int argc, char ** argv) {
 
