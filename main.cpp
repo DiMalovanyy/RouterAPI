ccad4283a58ae77b729268dd90b3d73bc27878d0
Dmytro Malovanyy <dmitrymalovanyy@gmail.com>
2021-06-20 20:02:45 +0300

    Logger init

--- main.cpp
+++ main.cpp
@@ -5,7 +5,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+//Other libs
+
+//Toml
 #include <toml++/toml.h>
+//Logger
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+
+void initLoggers() {
+    //Stdcout logger
+    auto consoleLogger = spdlog::stdout_color_mt("console");
+    spdlog::get("console") -> info("Logger \"{0}\" init successfully", "console");
+    
+    //File logger
+    //TODO: add file logger
+}
 
 
 
@@ -14,7 +32,7 @@ struct APIParams {
     const char* port;
 };
 
-void parseTomlConfig( const char* confiPath ) {
+void parseTomlConfig( const char* confiPath, bool logParams = false) {
     toml::table table;
     try {
         table = toml::parse_file( confiPath );
@@ -22,13 +40,23 @@ void parseTomlConfig( const char* confiPath ) {
         std::cerr << "Some errors while parsing toml occured" << std::endl;
         exit(EXIT_FAILURE);
     }
+    spdlog::get("console") -> info("Toml config was parsed successfully from the path \"{0}\"", confiPath);
+    if ( logParams ) {
+        spdlog::get("console") -> info("Toml parsed parms");
+        for (int i = 0; i < table.size(); ++i) {
+            //TODO: params info
+//            spdlog::get("console") -> info("\t{0}: {1}", table.)
+        }
+        
+    }
+    
 }
 
 
 
 int main(int argc, char ** argv) {
-
-
-	puts("Hello Api\n");
+    initLoggers();
+    parseTomlConfig("./apiConfig.toml");
+    
 	return 0;
 }
